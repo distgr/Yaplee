1f54afb665688168ad1f113733a725b3e04f400d
Matin Najafi <matin.najafi@gmail.com>
2021-11-13 08:45:19 +0330

    Reformat codes

--- yaplee/app.py
+++ yaplee/app.py
@@ -8,9 +8,7 @@ from yaplee.errors import (SetError, SyncError, InitialNotFound,
 class app:
     def __init__(self):
         if(os.geteuid() and os.name == 'posix'):
-            sys.exit(
-                print('Warning: Please run app in sudo')
-            )
+            sys.exit(print('Warning: Please run app in sudo'))
         self.__metabase = {
             'actions': {},
             'config': {'debug': True, 'sync': None, 'port': 8989, 'opentab': False},
@@ -22,20 +20,26 @@ class app:
         self.is_debug = self.__metabase['config']['debug']
 
     def init(self, initial_func):
-        self.__metabase['actions']['initial'], self.__on_init = initial_func, True
+        self.__on_init = True
+        self.__metabase['actions']['initial'] = initial_func
         initial_func()
         self.__on_init = False
 
     def start(self):
         server = Server(self.__metabase)
+
         if not 'initial' in self.__metabase['actions']:
             raise InitialNotFound('Initial function is required for any yaplee project')
+
         if not self.__metabase['config']['debug']:
             return
+
         if self.__metabase['config']['sync']:
             return
+
         print('Starting yaplee debug development server...')
         print(((' '*3)+'- ')+'Tip: You can use `CTRL + C` to close development server')
+        
         port = self.__metabase['config']['port']
         if not server.is_port_open():
             raise PortOnUse('{} port is on use, make sure another yaplee server is not started or {}'.format(str(port),
@@ -50,40 +54,55 @@ class app:
     def auto_sync(self, fw):
         if fw.lower() not in syncable:
             raise SyncError('Yaplee cannot sync with \'{}\''.format(fw))
+
         if os.environ.get('RUN_MAIN') != 'true':
             print('[Yaplee] : Preparing your application...')
+            
             server = Server(self.__metabase)
             settings_py_path = os.path.join(self.user_path, self.user_path.split(os.path.sep)[-1], 'settings.py')
+
             with open(settings_py_path, 'r+') as settings_py_data:
                 settings_py_data = settings_py_data.read()
+
             with open(settings_py_path, 'w+') as settings_py:
                 if 'import os' not in settings_py_data:
-                    settings_py_data = settings_py_data.replace('from pathlib import Path', "import os\nfrom pathlib import Path")
+                    settings_py_data = settings_py_data.replace(
+                        'from pathlib import Path',
+                        "import os\nfrom pathlib import Path"
+                    )
                 if 'yaplee_dir' not in settings_py_data:
-                    settings_py_data = settings_py_data.replace("'DIRS': [],", "'DIRS': [os.environ.get('yaplee_dir')],")
+                    settings_py_data = settings_py_data.replace(
+                        "'DIRS': [],",
+                        "'DIRS': [os.environ.get('yaplee_dir')],"
+                    )
                 settings_py.write(settings_py_data)
+
             os.environ['yaplee_dir'] = server.temp_path
 
     def template(self, template_path, **kwargs):
         if not os.path.isfile(template_path):
             raise TemplateNotFound('Template \'{}\' does not exist in your path'.format(template_path))
+
         def wrapper(func):
             template_meta = func()
             if type(template_meta) is not dict:
                 raise UnknownTemplateValue(
                     'Your template `{}` returns a non-dict value'.format(template_path)
                 )
+
             name = None if 'name' not in kwargs else kwargs['name']
             if template_path+'-_-' in self.__metabase['templates']:
                 raise TemplateExist(
                     'You have already added a template ('+template_path+'), '
                     'you can add a special name to your template by adding `name=\'...\'` to template decorator'
                 )
+
             elif template_path+'-_-'+str(name) in self.__metabase['templates']:
                 raise TemplateExist(
                     'You have already added a template with '+template_path+' and ('+str(name)+') special name, '
                     'please change your template special name'
                 )
+
             self.__metabase['templates'][template_path+'-_-'+('' if not name else name)] = {
                 'func': func,
                 'name': name,
--- yaplee/server.py
+++ yaplee/server.py
@@ -27,13 +27,16 @@ class Server:
         local_connection = ('127.0.0.1', self.port)
         port_open = a_socket.connect_ex(local_connection)
         a_socket.close()
+
         return not (not port_open)
 
     def __gen_yaplee_temp(self):
         self.tempuuid = uuid.uuid1().hex[:15]
         path = os.path.join(tempfile.gettempdir(), self.tempuuid)
+
         if not os.path.isdir(path):
             os.mkdir(path)
+
         return self.tempuuid, path
 
     def start(self):
@@ -85,17 +88,21 @@ class Server:
             elif 'style' in meta['meta']:
                 if type(meta['meta']['style']) is str:
                     styles = [meta['meta']['style']]
+
                 elif type(meta['meta']['style']) is list:
                     styles = meta['meta']['style']
+
                 else:
                     raise UnknownTemplateValue(
                         'template style must be list or string (one style)'
                     )
+
                 tag_loc, tags = 'head', {
                     str(random.randint(111111, 999999)):BeautifulSoup('', 'html.parser').new_tag(
                         'link', rel='stylesheet', href=style
                     ) for style in styles
                 }
+
                 for style in styles:
                     shutil.copy(
                         style,
@@ -105,8 +112,10 @@ class Server:
             with open(template_to_copy, 'r+') as file:
                 template_data = file.read()
                 soup = BeautifulSoup(template_data, 'html.parser')
+
                 for tagname, tag in tags.items():
                     soup.find(tag_loc).append(tag)
+
                 for funcname, function in js_functions.items():
                     unique_id = str(uuid.uuid1()).split('-')[0]
                     soup.html.append(soup.new_tag('script', id=unique_id))
@@ -115,6 +124,7 @@ class Server:
                         str(JSFunc(function))+
                         ' }'
                     )
+
                 file.truncate(0)
                 file.write(soup.prettify())
                 del file
@@ -122,10 +132,12 @@ class Server:
             generated_files.append(to_copy_path)
 
         if 'index.html' not in generated_files:
+
             with open(os.path.join(self.module_path, 'assets', 'no-index.html.py'), 'r+') as file:
                 nohtml_base = file.read()
                 file.close()
                 del file
+
             nohtml_base = nohtml_base.replace('{% avaliable_paths %}',
                 '' if not self.templates else
                 '<h4>Avaliable paths : {}</h4>'.format(
@@ -135,13 +147,16 @@ class Server:
                     ) for i, j in self.templates.items()])
                 )
             )
+
             with open(os.path.join(self.temp_path, 'index.html'), 'w+') as file:
                 file.write(nohtml_base)
 
         if self.opentab:
             webbrowser.open('http://127.0.0.1:{}/'.format(str(self.port)))
             time.sleep(1)
+
         yield self.temp_uuid, self.temp_path
+        
         subprocess.run(
             ('python3' if os.name == 'posix' else 'python')+' -m http.server '+str(self.port)+' --bind 127.0.0.1 --directory "'+self.temp_path+'"',
             shell=True
